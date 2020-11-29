cbd8e6b8b8a94a02d32533e3c1ae26891347aa71
Todoo S.A.S <info@todoo.co>
2020-11-29 09:45:54 -0500

    Auth lougout

---
+++ oi_session/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inactive Sessions Timeout",
+
+    'summary': """""",
+
+    'description': """
+        
+    """,
+
+    'author': "Openinside",
+    'website': "https://www.open-inside.com",
+    "license": "OPL-1",
+    "price" : 29.99,
+    "currency": 'EUR',    
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '13.0.1.1.8',
+
+    # any module necessary for this one to work correctly
+    'depends': ['mail'],
+
+    # always loaded
+    'data': [
+        'data/ir_cron.xml',
+        'security/ir.model.access.csv',
+        'view/ir_session.xml',
+        'view/action.xml',
+        'view/menu.xml'
+    ],
+    
+    'post_init_hook' : 'post_init_hook',
+    
+    'external_dependencies' : {
+        'python' : [],
+    },
+    'odoo-apps' : True,
+    'auto_install': True,
+    'images':[
+            'static/description/cover.png'
+        ],     
+}
