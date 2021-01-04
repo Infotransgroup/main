ec488d40f5c5147868a9ebc188f4c9f2d278acea
blara <bl@todoo.co>
2021-01-04 11:05:14 -0500

    einv [FIX] default email template in invoice

--- l10n_co_e_invoicing_comfiar/models/account_invoice.py
+++ l10n_co_e_invoicing_comfiar/models/account_invoice.py
@@ -178,29 +178,32 @@ class AccountInvoice(models.Model):
 			message loaded by default
 		"""
 		self.ensure_one()
-		template = self.env.ref('l10n_co_e_invoicing_comfiar.email_template_for_einvoice')
+		template = False
+		if self.company_country_code == 'CO':
+			template = self.env.ref('l10n_co_e_invoicing_comfiar.email_template_for_einvoice')
 		# template = self.env.ref('account.email_template_edi_invoice', raise_if_not_found=False)
-		dian_document = self.dian_document_lines.filtered(lambda x: x.state == 'done')
-		if len(dian_document) > 1:
-			raise ValidationError('Hay mas de un documento DIAN en estado "Hecho", validar que solo exista uno')
-		elif len(dian_document) == 1:
-			
-			xml_attachment_file = self.env['ir.attachment'].create({
-				'name': dian_document.xml_filename,
-				'type': 'binary',
-				'datas': dian_document.xml_file}) #b64encode(xml_without_signature.encode()).decode("utf-8", "ignore")})
+		if template:
+			dian_document = self.dian_document_lines.filtered(lambda x: x.state == 'done')
+			if len(dian_document) > 1:
+				raise ValidationError('Hay mas de un documento DIAN en estado "Hecho", validar que solo exista uno')
+			elif len(dian_document) == 1:
+				
+				xml_attachment_file = self.env['ir.attachment'].create({
+					'name': dian_document.xml_filename,
+					'type': 'binary',
+					'datas': dian_document.xml_file}) #b64encode(xml_without_signature.encode()).decode("utf-8", "ignore")})
 
-			pdf_attachment_file = self.env['ir.attachment'].create({
-				'name': dian_document.pdf_filename,
-				'type': 'binary',
-				'datas': dian_document.pdf_file})
-			# pdf_attachment = self.env['ir.attachment'].create({
-			# 	'name': self.name + '.pdf',
-			# 	'type': 'binary',
-			# 	'datas': self._get_pdf_file()})
+				pdf_attachment_file = self.env['ir.attachment'].create({
+					'name': dian_document.pdf_filename,
+					'type': 'binary',
+					'datas': dian_document.pdf_file})
+				# pdf_attachment = self.env['ir.attachment'].create({
+				# 	'name': self.name + '.pdf',
+				# 	'type': 'binary',
+				# 	'datas': self._get_pdf_file()})
 
-			template.attachment_ids = [(6, 0, [(pdf_attachment_file.id),
-											   (xml_attachment_file.id)])]
+				template.attachment_ids = [(6, 0, [(pdf_attachment_file.id),
+												(xml_attachment_file.id)])]
 
 		lang = get_lang(self.env)
 		if template and template.lang:
@@ -211,14 +214,15 @@ class AccountInvoice(models.Model):
 		ctx = dict(
 			default_model='account.move',
 			default_res_id=self.id,
-			default_use_template=bool(template),
-			default_template_id=template and template.id or False,
 			default_composition_mode='comment',
 			mark_invoice_as_sent=True,
 			custom_layout="mail.mail_notification_paynow",
 			model_description=self.with_context(lang=lang).type_name,
 			force_email=True
 		)
+		if template:
+			ctx['default_use_template'] = bool(template)
+			ctx['default_template_id'] = template and template.id or False,
 
 		return {
 			'name': _('Send Invoice'),
